21c07b3f4c8634b001b3f9ba8afa2281f9402c13
achraftarsi-collab <achraftarsi@gmail.com>
2026-05-17 06:40:52 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import DisplayScreen from './DisplayScreen.jsx'
+
+const path = window.location.pathname;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {path === '/display' ? <DisplayScreen /> : <App />}
   </React.StrictMode>,
 )
